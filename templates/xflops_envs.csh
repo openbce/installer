bb277112c7afaa00a6ddc01d50a9c029172f9482
Klaus Ma <klaus@xflops.cn>
2022-06-29 14:49:49 +0800

    Update installer to kubeadm+ansible
    Signed-off-by: Klaus Ma <klaus@xflops.cn>

--- templates/xflops_envs.csh
+++
@@ -1,3 +0,0 @@
-setenv XFLOPS_HOME={{ .HomeDir }}
-setenv XFLOPS_WORK_DIR={{ .WorkDir }}
-setenv PATH=${XFLOPS_HOME}/bin:${XFLOPS_HOME}/sbin:${PATH}
--- templates/xflops_envs.sh
+++
@@ -1,3 +0,0 @@
-export XFLOPS_HOME={{ .HomeDir }}
-export XFLOPS_WORK_DIR={{ .WorkDir }}
-export PATH=${XFLOPS_HOME}/bin:${XFLOPS_HOME}/sbin:${PATH}
